e24ed1946885f55f5744897dd6471d5d1ac390c0
Francis Roy Balasabas <francisroy1124@gmail.com>
2020-11-22 01:17:05 -0500

    changing position of json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 // Required Modules
 const fs = require("fs");
-const notesData = require("../db/db.json");
+const notesData = require("../Develop/db/db.json");
 
 
 module.exports = function(app){
@@ -12,7 +12,7 @@ module.exports = function(app){
         notes = JSON.stringify(notes);
         console.log (notes);
         // Writes String back to db.json
-        fs.writeFileSync("../db/db.json", notes, function(err){
+        fs.writeFileSync("../Develop/db/db.json", notes, function(err){
             if (err) {
                 return console.log(err);
             }
